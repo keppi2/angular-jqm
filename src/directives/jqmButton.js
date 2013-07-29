55f5f7b4d6fdf562dafc73a18a60345f16a05848
Marius Keppler[Opitz] <marius.keppler@opitz-consulting.com>
2013-07-29 14:42:12 +0200

    feat(jqmButton): Initial beta Version

--- src/directives/jqmButton.js
+++ src/directives/jqmButton.js
@@ -8,25 +8,13 @@
  * 
  * The jqm-button can be used on <a></a> and <button /> Tags
  * @param {string=} jqmButton Value "button" defines if this directive is set on a <button /> tag
- * @param {string=} disabled Whether this button is disabled.
- * @param {string=} mini Whether this checkbox is mini.
- * @param {string=} iconpos The position of the icon for this element. "left" or "right".
+ * @param {string=} jqmButtonDisabled 
+ * @param {string=} jqmButtonIcon
+ * @param {string=} jqmButtonIconPos
+ * @param {string=} mini
  *
  * @example
-<example module="jqm">
-  <file name="index.html">
-    <div jqm-checkbox ng-model="checky">
-      My value is: {{checky}}
-    </div>
-    <div jqm-checkbox mini="true" iconpos="right" ng-model="isDisabled">
-      I've got some options. Toggle me to disable the guy below.
-    </div>
-    <div jqm-checkbox disabled="{{isDisabled ? 'disabled' : ''}}" 
-      ng-model="disably" ng-true-value="YES" ng-false-value="NO">
-      I can be disabled! My value is: {{disably}}
-    </div>
-  </file>
-</example>
+
  */
 jqmModule.directive('jqmButton', [function () {
     return {
@@ -34,7 +22,8 @@ jqmModule.directive('jqmButton', [function () {
         transclude: true,
         templateUrl: 'templates/jqmButton.html',
         scope: {
-            mini: '@'
+            mini: '@',
+            theme: '@'
         },
         require: ['?jqmControlGroupCtrl'],
         link: function (scope, element, attr, ctrl){
@@ -45,47 +34,92 @@ jqmModule.directive('jqmButton', [function () {
           scope.isMini = isMini;
           scope.isDisabled = isDisabled;
           scope.isIcon = isIcon;
+          scope.isButton = isButton;
+          scope.isSubmitButton = isSubmitButton;
+          scope.isResetButton = isResetButton;
+          scope.theme = scope.$theme || 'c';
           
           function isMini() {
             return scope.mini || (jqmControlGroupCtrl && jqmControlGroupCtrl.$scope.mini);
           }
         
           function isDisabled(){
-            return attr.disabled === "disabled" || element.hasClass("ui-disabled");
+            return attr.jqmButtonDisabled === "" || element.hasClass("ui-disabled");
           }
 
           function isIcon(){
             return attr.jqmButtonIcon; 
           }
-
+          
+          function getIconPos(){
+           if(attr.jqmButtonIconpos)
+             return attr.jqmButtonIconpos;
+            else
+             return "left";
+          }
+ 
           function toggleMouseOver(){
             element.toggleClass("ui-btn-up-"+scope.$theme);
             element.toggleClass("ui-btn-hover-"+scope.$theme);
           }
           
+          function toggleMouseClick(){
+            element.toggleClass("ui-btn-down-"+scope.$theme);
+            element.toggleClass("ui-btn-up-"+scope.$theme);
+          }
           
- 
+          function toggleMouseLeave(){
+            //there will be no mouseup event if mouse left before
+            if(element.hasClass("ui-btn-down-"+scope.$theme)){
+              toggleMouseClick();        
+            }
+            
+            toggleMouseOver();
+          }
           
-          var hasExplicitTheme = scope.hasOwnProperty('$theme');
-          if (!hasExplicitTheme) {
-              scope.$theme = 'c';
+          function isButton(){
+            return attr.jqmButton.toLowerCase() === "button" || isSubmitButton() || isResetButton();
+          } 
+          function isSubmitButton(){
+            return attr.jqmButton.toLowerCase() === "submit";
+          }      
+          function isResetButton(){
+            return attr.jqmButton.toLowerCase() === "reset" ;
           }
 
-          element.addClass("ui-btn ui-shadow ui-btn-corner-all ui-btn-up-"+scope.$theme+" "+((isMini())?"ui-mini ":" ") +((isIcon())?"ui-btn-icon-left ":" ") );
-          element.bind('mouseenter mouseleave', toggleMouseOver);
+        
 
-       
-          if(attr.jqmButton.toLowerCase() === "button"){
-            content = element.text();
-            element.append('<button class="ui-btn-hidden" data-disabled="'+((scope.isDisabled())?"true":"false")+'">'+content+'</button>');
+          element.addClass("ui-btn ui-shadow ui-btn-corner-all ui-btn-up-"+scope.$theme+" "+
+                           ((isMini())?"ui-mini ":" ") +
+                           ((isIcon())?"ui-btn-icon-"+getIconPos()+" ":" ") + 
+                           ((isSubmitButton())?"ui-submit":"") );
+          element.bind('mouseenter', toggleMouseOver);
+          element.bind('mouseleave', toggleMouseLeave);
+          element.bind('mousedown mouseup', toggleMouseClick);
+
+         
+          if(scope.isIcon()){
+              element.children(0).append('<span class="ui-icon ui-icon-'+attr.jqmButtonIcon+' ui-icon-shadow">&nbsp;</span>');
+          }
+          
+          if(scope.isButton()){
+            var type = "";
+            
+            if(isSubmitButton())
+              type = 'type="submit" ';
+            else if(isResetButton())
+              type = 'type="reset" ';
+              
+            var content = element.text();
+            element.append('<button '+type+'class="ui-btn-hidden" data-disabled="'+((scope.isDisabled())?"true":"false")+'">'+content+'</button>');
           }
           
           if(scope.isDisabled())
             element.addClass("ui-disabled");
           
-          if(scope.isIcon()){
-            element.children(0).append('<span class="ui-icon ui-icon-delete ui-icon-shadow">&nbsp;</span>');
-          }
+  
+
+          
         }
     }
 }]);
